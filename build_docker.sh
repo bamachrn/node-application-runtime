af3bd4cab3291967ed606fa31e72ac4f6cdb5f64
Bamacharan Kundu <bamachrn@gmail.com>
2016-05-16 22:43:42 +0530

    Added build steps to build docker script

--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,3 @@
 sudo docker build -t nodejs:ss .
+git clone https://bamachrn@bitbucket.org/krish_tech/krishtech_servicestation.git
+sudo docker run -v `pwd`/krishtech_servicestation:/nodeApplication nodejs:ss /firstRun.sh
---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+sudo docker run -v `pwd`/krishtech_servicestation:/nodeApplication nodejs:ss /runApp.sh
