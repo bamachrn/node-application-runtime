b7e6f4302ceddfb0c0b318ceb8c7198561e91037
Bamacharan Kundu <bamachrn@gmail.com>
2016-05-22 19:44:11 +0530

    Port exported to 3000 of local machine

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker stop mariadb-server
 docker rm mariadb-server
 docker run --name mariadb-server -v `pwd`/nodeData:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mariadb:5
-docker run -v `pwd`/krishtech_servicestation:/nodeApplication -p 9090:3000 nodejs:ss /runApp.sh
+docker run -it --rm -v `pwd`/krishtech_servicestation:/nodeApplication -p 3000:3000 nodejs:ss /runApp.sh
