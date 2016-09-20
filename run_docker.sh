762bb86445219b8b9846acd0124ddeded500d0d7
Bamacharan Kundu <bamachrn@gmail.com>
2016-09-20 11:42:42 +0530

    Changed port for application

--- run_docker.sh
+++ run_docker.sh
@@ -5,4 +5,4 @@ cd krishtech_servicestation
 git pull origin development
 sed -i s/127.0.0.1/`ip -4 route get 8.8.8.8|rev|cut -d " " -f2|rev|head -1`/g config/database.json
 cd ../
-docker run -it --rm -v `pwd`/krishtech_servicestation:/nodeApplication -p 3000:3000 nodejs:ss /runApp.sh
+docker run -it --rm -v `pwd`/krishtech_servicestation:/nodeApplication -p 3095:3095 nodejs:ss /runApp.sh
